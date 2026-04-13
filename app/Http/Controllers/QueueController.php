8a05144f5a28ce34e1d9fb4b66ad8f7222dc5efd
izar <nizar.nizar.nizar.26@gmail.com>
2026-04-13 08:29:09 +0700

    antrian

--- app/Http/Controllers/QueueController.php
+++ app/Http/Controllers/QueueController.php
@@ -10,15 +10,27 @@ class QueueController extends Controller
 {
     public function index()
     {
+        $mapTransaction = function ($transaction) {
+            return [
+                'id' => $transaction->id,
+                'invoice_code' => $transaction->invoice_code,
+                'customer' => $transaction->customer ? [
+                    'name' => optional($transaction->customer->user)->name ?? 'Pelanggan'
+                ] : null,
+            ];
+        };
+
         $processing = Transaction::where('status', 'process')
-            ->with('customer:id,name')
+            ->with(['customer.user'])
             ->orderBy('updated_at', 'desc')
-            ->get();
+            ->get()
+            ->map($mapTransaction);
 
         $ready = Transaction::where('status', 'ready')
-            ->with('customer:id,name')
+            ->with(['customer.user'])
             ->orderBy('updated_at', 'desc')
-            ->get();
+            ->get()
+            ->map($mapTransaction);
 
         return Inertia::render('Queue/Index', [
             'processing' => $processing,
